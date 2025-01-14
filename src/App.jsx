7679360f9d008431bddd34bb4fcadc8b4cc823b2
sbtech24 <bajomosemilore@gmail.com>
2025-01-15 00:08:24 +0100

    Html Template

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ const App = () => {
 
               <p>
                 This item will be sent to your system automatically after
-                purchase{" "}
+                purchase
               </p>
             </div>
           </div>
@@ -66,6 +66,258 @@ const App = () => {
             </div>
           </div>
         </section>
+
+        <section>
+          <div>
+            <h3>ESRB rating</h3>
+
+            <div>
+              <img src="" alt="" />
+
+              <div>
+                <p>Fantasy Violence,Use of Alcohol,Use of Tobacco,
+                  Simulated Gambling, Mild Language,Mild Blood 
+
+                </p>
+
+                <p>Users Interact </p>
+              </div>
+              
+            </div>
+          </div>
+
+
+
+          <div>
+            <div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+
+            <div>
+              <ul>
+                <li>TV</li>
+                <li>Tabletop</li>
+                <li>Handheld</li>
+              </ul>
+            </div>
+          </div>
+        </section>
+
+
+        <section>
+          <div>
+            <ul>
+             <li>Release date</li>
+             <li>No of players</li>
+             <li>Genre</li>
+             <li>Publisher</li>
+             <li>Developer</li>
+             <li>ESRB rating</li>
+             <li>Supported play modes </li>
+             <li>Game file size</li>
+             <li>Supported languages </li>
+            </ul>
+          </div>
+
+
+          <div>
+            <ul>
+              <li>October ,2017 </li>
+              <li>
+                <ul>
+                  <li>Single System(1)</li>
+                  <li>Local wireless (1)</li>
+                  <li>online(1-4)</li>
+                </ul>
+              </li>
+
+
+              <li>
+                <ul>
+                  <li>Role-playing</li>
+                  <li>Simulation</li>
+                </ul>
+              </li>
+
+              <li>ConcernedApe</li>
+              <li>ConcernedApe</li>
+              <li>Everyone10+</li>
+              <li>TV mode,Tabletop mode,Handheld mode</li>
+              <li>1.5GB </li>
+              <li>English,French,German,Italian,Japanese,
+                Korean, Portuguese, Rusian,Simplified Chinese, Spanish, Traditional Chinese
+              </li>
+            </ul>
+          </div>
+        </section>
+
+
+        <section>
+          <div>
+            <h2>Online</h2>
+            <p>Play online,access classics</p>
+
+            <p>This game supports: Online Play Save Data Cloud </p>
+
+            <button>Learn more </button>
+          </div>
+
+          <div>
+            {/* man holding a switch  */}
+            <img src="" alt="" />
+          </div>
+        </section>
+
+        {/* More Like this carousel component  */}
+        <section>
+
+        </section>
+
+        <section>
+          <div>
+            <p>WARNING: If you have epilepsy
+               or have had seizures or other unusual reactions to flashing lights or patterns, consult a doctor before playing video games. All users should read the Health and Safety Information available in the system settings before using this software.
+              
+            </p>
+
+
+          </div>
+          <div>
+            <p>Nintendo Switch Online membership (sold separately) and Nintendo Account required for online play. Not available in all countries. Internet access required for online features. Terms apply. 
+
+            </p>
+
+          </div>
+          <p>Stardew Valley © ConcernedApe LLC 2016-2020. All rights reserved.
+
+          </p>
+        </section>
+
+        <footer>
+          <div>
+            {/* Nintendo log  */}
+            <img src="" alt="" />
+
+            {/* Social Icons */}
+            <div>
+              <img src="" alt="" />
+              <img src="" alt="" />
+              <img src="" alt="" />
+            </div>
+          </div>
+
+          <section>
+            <div>
+              <div>
+                <h2>About Nintendo</h2>
+
+                <ul>
+                  <li>Careers</li>
+                  <li>Corporate Social Responsibility</li>
+                </ul>
+              </div>
+
+              <div>
+                <h2>Shop</h2>
+
+                <ul>
+                  <li>Games</li>
+                  <li>Hardware</li>
+                  <li>Mercahandise</li>
+                  <li>Sales & deals</li>
+                  <li>Exclusives</li>
+                  <li>Online service</li>
+                  <li>Nintendo NY store</li>
+                </ul>
+              </div>
+
+              <div>
+                <h2>Orders</h2>
+
+                <ul>
+                  <li>Order details</li>
+                  <li>Shipping info</li>
+                  <li>Returns & exchanges </li>
+                  <li>FAQ</li>
+                </ul>
+              </div>
+
+              <div>
+                <h2>Support</h2>
+
+                <ul>
+                  <li>Nintendo Switch</li>
+                  <li>Nintendo Account</li>
+                  <li>Other systems</li>
+                  <li>Repairs</li>
+                  <li>Nintendo product recycling</li>
+                  <li>Warranty</li>
+                  <li>Licensed product information</li>
+                </ul>
+              </div>
+
+              <div>
+                <h2>Parents</h2>
+
+                <ul>
+                  <li>Info for parents </li>
+                  <li>Parental controls</li>
+                </ul>
+              </div>
+
+              <div>
+                <h2>Privacy</h2>
+                <ul>
+                  <li>Privacy policy </li>
+                  <li>Cookies and interst-based ads</li>
+                </ul>
+              </div>
+
+              <div>
+                <h2>Community</h2>
+
+                <ul>
+                  <li>Community guidelines</li>
+                  <li>Online safety principles </li>
+                </ul>
+              </div>
+
+              <div>
+                <h2>Documents & policies</h2>
+
+                <ul>
+                  <li>Health & safety precautions</li>
+                  <li>Wireless regulations </li>
+                  <li>Supply chain transperancy</li>
+                </ul>
+              </div>
+
+              {/* Bottom Image Flex-end  */}
+              <div></div>
+
+            </div>
+          </section>
+
+          <section>
+            <div>
+              <p>Nintendo. Games are property of their respective owners.
+                Nintendo of America Inc. Headquaters are in Redmond, 
+                Washington,USA
+              </p>
+            </div>
+
+            <div>
+              <ul>
+                <li>Contact us </li>
+                <li>Website feedback</li>
+                <li>Terms of use </li>
+                <li>English(united states)</li>
+              </ul>
+            </div>
+          </section>
+        </footer>
       </main>
     </div>
   );
